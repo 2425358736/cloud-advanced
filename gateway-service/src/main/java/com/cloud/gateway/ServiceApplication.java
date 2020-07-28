03e0e1d6ddc0a3616712f7e0d1926bee105f7144
lzq <576105854@qq.com>
2020-07-28 18:05:12 +0800

    v1.0

---
+++ gateway-service/src/main/java/com/cloud/gateway/ServiceApplication.java
@@ -0,0 +1,28 @@
+package com.cloud.gateway;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author 刘志强
+ * @date 2020/7/16 13:21
+ * @EnableEurekaClient 开启Eureka 客户端
+ */
+@SpringBootApplication
+@Slf4j
+@EnableEurekaClient
+public class ServiceApplication implements CommandLineRunner {
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceApplication.class,args);
+    }
+
+    @Override
+    public void run(String... args) {
+        log.info("gateway服务器已启动");
+    }
+}
