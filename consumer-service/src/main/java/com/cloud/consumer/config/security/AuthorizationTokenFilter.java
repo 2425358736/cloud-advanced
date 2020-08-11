84821982c5600ff3bd8eea3a2dbd8cd2c81897e8
lzq <576105854@qq.com>
2020-08-11 10:48:29 +0800

    v1.0

--- consumer-service/src/main/java/com/cloud/consumer/config/security/AuthorizationTokenFilter.java
+++ consumer-service/src/main/java/com/cloud/consumer/config/security/AuthorizationTokenFilter.java
@@ -1,9 +1,5 @@
 package com.cloud.consumer.config.security;
 
-import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.JSONObject;
-import com.cloud.common.core.result.AjaxResult;
-import com.cloud.common.core.result.HttpStatus;
 import com.cloud.consumer.config.security.model.User;
 import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -18,7 +14,6 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.PrintWriter;
 
 /**
  * @author admin
--- consumer-service/src/main/java/com/cloud/consumer/config/security/SecurityConfig.java
+++ consumer-service/src/main/java/com/cloud/consumer/config/security/SecurityConfig.java
@@ -15,10 +15,22 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 /**
  * @author 刘志强
  * @date 2020/7/21 09:34
+ * * @EnableGlobalMethodSecurity(securedEnabled=true)
+ * * 开启@Secured 注解过滤权限
+ * *
+ * * @EnableGlobalMethodSecurity(jsr250Enabled=true)
+ * * 开启@RolesAllowed 注解过滤权限
+ * *
+ * * @EnableGlobalMethodSecurity(prePostEnabled=true)
+ * * 使用表达式时间方法级别的安全性 4个注解可用
+ * * -@PreAuthorize 在方法调用之前,基于表达式的计算结果来限制对方法的访问
+ * * -@PostAuthorize 允许方法调用,但是如果表达式计算结果为false,将抛出一个安全性异常
+ * * -@PostFilter 允许方法调用,但必须按照表达式来过滤方法的结果
+ * * -@PreFilter 允许方法调用,但必须在进入方法之前过滤输入值
  */
 @Configuration
 @EnableWebSecurity
-@EnableGlobalMethodSecurity(prePostEnabled = true,securedEnabled=true)
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     private final UserDetailsServiceImpl userDetailsService;
@@ -56,7 +68,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 // /authenticated 下的请求允许认证过的用户访问
                 .antMatchers("/authenticated/**").authenticated()
                 // 其余的请求允许所有用户无条件访问
-                 .antMatchers("/login","/getConsumer").permitAll()
+                .antMatchers("/login", "/getConsumer").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .csrf().disable();
